f0df5403cce6f09b8590c1c0ec8ac2e9a0a8dd8a
Arya <aryasanj2000@gmail.com>
2024-09-09 22:06:02 -0700

    removing the read more

--- scripts.js
+++ scripts.js
@@ -6,19 +6,3 @@ document.querySelectorAll('nav ul li a').forEach(anchor => {
         document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
     });
 });
-
-function showMore() {
-    var moreContent = document.querySelectorAll('.more-content');
-    var readMoreLink = document.getElementById('read-more');
-
-    // Toggle the visibility of the hidden list items
-    moreContent.forEach(function(item) {
-        if (item.style.display === "none") {
-            item.style.display = "list-item";  // Show the hidden list items
-            readMoreLink.innerText = "Read less";
-        } else {
-            item.style.display = "none";  // Hide the list items again
-            readMoreLink.innerText = "Read more";
-        }
-    });
-}
