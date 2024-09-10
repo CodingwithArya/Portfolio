452e05381231db39150dd87064702a1dbd7cad16
Arya <aryasanj2000@gmail.com>
2024-09-09 21:49:59 -0700

    Adding read more functionality to website

--- scripts.js
+++ scripts.js
@@ -7,3 +7,15 @@ document.querySelectorAll('nav ul li a').forEach(anchor => {
     });
 });
 
+function showMore() {
+    var moreContent = document.getElementById("more-content");
+    var readMoreLink = document.getElementById("read-more");
+
+    if (moreContent.style.display === "none") {
+        moreContent.style.display = "block";
+        readMoreLink.innerText = "Read less";
+    } else {
+        moreContent.style.display = "none";
+        readMoreLink.innerText = "Read more";
+    }
+}
